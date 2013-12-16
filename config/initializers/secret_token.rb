eb77f2daa2a00ea863b4037372a700d95643ace6
Bradley Geddes <bradtg@gmail.com>
2013-12-16 08:40:14 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PecosSite::Application.config.secret_key_base = '098bfda13e28897b202104f59d1ab85d30df64cdc0d0e5ee30630188e712edb6be97a0c50240cc4f7c14c57d9a9c0df4f88ba8ff731348eb6bd97b99838e633c'
